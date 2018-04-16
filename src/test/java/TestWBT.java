2132cc7c3e7d859d8fcfc9d7e4e8a4b13d6756f9
budupaul <budupaul@yahoo.com>
2018-04-16 17:49:02 +0300

    New wbt tests

--- src/test/java/TestWBT.java
+++ src/test/java/TestWBT.java
@@ -6,25 +6,85 @@ import org.junit.Test;
 
 import junit.framework.TestCase;
 import model.Laboratory;
-import model.Student;
 import validator.Validator;
 
 public class TestWBT extends TestCase {
 
 	@Test
-	public void testLaboratoryDate() throws ParseException {
-		Validator val = new Validator();
+	public void testLaboratoryDateGood() throws ParseException {
 		Laboratory lab = new Laboratory(1, "1/1/2019", 1, "asdf4321");
 		assert(Validator.validateLaboratory(lab) == true);
+		
+	}
+	
+	@Test
+	public void testLaboratoryDateError() throws ParseException {
 		try{
-			lab = new Laboratory(1, "1/1", 1, "asdf4321");
+			Laboratory lab = new Laboratory(1, "1/1", 1, "asdf4321");
 			assert(false);
 		}
 		catch(ParseException e){
 			assert(true);
 		}
-		lab = new Laboratory(1, "1/1/2017", 1, "asdf4321");
+	}
+	
+	@Test
+	public void testLaboratoryDateBad() throws ParseException{
+		Laboratory lab = new Laboratory(1, "1/1/2017", 1, "asdf4321");
 		assert(Validator.validateLaboratory(lab) == false);
+		
+	}
+	
+	@Test
+	public void testLaboratoryNumberGood() throws ParseException{
+		Laboratory lab = new Laboratory(1, "1/1/2019", 1, "asdf4321");
+		assert(Validator.validateLaboratory(lab) == true);
+		
+	}
+	
+	@Test
+	public void testLaboratoryNumberBad() throws ParseException{
+		Laboratory lab = new Laboratory(0, "1/1/2019", 1, "asdf4321");
+		assert(Validator.validateLaboratory(lab) == false);
+		
+	}
+	
+	@Test
+	public void testProblemNumberGood() throws ParseException{
+		Laboratory lab = new Laboratory(2, "1/1/2019", 1, "asdf4321");
+		assert(Validator.validateLaboratory(lab) == true);
+		
+	}
+	
+	@Test
+	public void testProblemNumberBad1() throws ParseException{
+		Laboratory lab = new Laboratory(3, "1/1/2019", 0, "asdf4321");
+		assert(Validator.validateLaboratory(lab) == false);
+		
+	}
+	
+	@Test
+	public void testProblemNumberBad2() throws ParseException{
+		Laboratory lab = new Laboratory(3, "1/1/2019", 11, "asdf4321");
+		assert(Validator.validateLaboratory(lab) == false);
+		
+	}
+	
+	@Test
+	public void testGradeGood() {
+		assert(Validator.validateGrade(2) == true);
+		
+	}
+	
+	@Test
+	public void testGradeBad1() {
+		assert(Validator.validateGrade(0) == false);
+		
+	}
+	
+	@Test
+	public void testGradeBad2() {
+		assert(Validator.validateGrade(11) == false);
 		
 	}
 
